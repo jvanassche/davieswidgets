e45983a1cc168b8f8a20c872a141cf5bc58e4027
John VanAssche <jvanassche@gmail.com>
2012-07-30 14:15:34 -0700

    Added payment data validations.  Added element hiding.  Hid Order Details and Order Payments by default.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,5 +2,19 @@ class Payment < ActiveRecord::Base
   attr_accessible :OrderID, :PaymentAmount, :PaymentDate, :PaymentMethodID, :Status
   belongs_to :payment_method, :foreign_key => 'PaymentMethodID'
   belongs_to :order, :foreign_key => 'OrderID'
+  validates :OrderID, :PaymentAmount, :PaymentDate, :presence => true
+  validates :PaymentAmount, :numericality => true
+  validate :order_id_exists
+	def order_id_exists
+		if Order.find_by_id(self.OrderID).nil?
+			errors.add(:base, "Invalid Order Specified")
+		end
+	end
 
+  validate :payment_method_exists
+	def payment_method_exists
+		if PaymentMethod.find_by_id(self.PaymentMethodID).nil?
+			errors.add(:base, "Invalid Payment Method specified.")
+		end
+	end
 end
