3c06c3695a888a1cba443c73f396da0517bb5671
John VanAssche <jvanassche@gmail.com>
2012-07-30 13:24:25 -0700

    Started adding payments frameworks.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,6 @@
 class Payment < ActiveRecord::Base
   attr_accessible :OrderID, :PaymentAmount, :PaymentDate, :PaymentMethodID, :Status
-  belongs_to :payment_method, foreign_key => 'PaymentMethodID'
-  belongs_to :order, foreign_key => 'OrderID'
+  belongs_to :payment_method, :foreign_key => 'PaymentMethodID'
+  belongs_to :order, :foreign_key => 'OrderID'
 
 end
